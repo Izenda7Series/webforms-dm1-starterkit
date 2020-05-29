8f1070482b953f2d367a45af5486cb27b22bfaa7
eric chang <eric.chang@izenda.com>
2020-05-29 13:07:58 -0400

    more update

--- WebFormsstarterKit_BE_Standalone/Models/Identity/IdentityModels.cs
+++ WebFormsstarterKit_BE_Standalone/Models/Identity/IdentityModels.cs
@@ -1,47 +1,8 @@
 ﻿using Microsoft.AspNet.Identity.EntityFramework;
-using System;
 using System.Data.Entity;
-using System.Threading.Tasks;
 
 namespace WebformsIntegratedBE_Standalone.Models
 {
-    public class ApplicationUserStore<TUser> : UserStore<TUser> where TUser : ApplicationUser
-    {
-        #region Properties
-        public int TenantId { get; set; } 
-        #endregion
-
-        #region CTOR
-        public ApplicationUserStore(DbContext context) : base(context)
-        { }
-        #endregion
-
-        #region Methods
-        public override Task CreateAsync(TUser user)
-        {
-            if (user == null)
-                throw new ArgumentNullException("user");
-            
-            TenantId = user.Tenant_Id.Value;
-            
-            return base.CreateAsync(user);
-        }
-
-        public override Task<TUser> FindByEmailAsync(string email)
-        {
-            return GetUserAggregateAsync(u => u.Email.ToUpper() == email.ToUpper() && u.Tenant_Id == TenantId);
-        }
-
-        public override Task<TUser> FindByNameAsync(string userName)
-        {
-            return GetUserAggregateAsync(u => u.UserName.ToUpper() == userName.ToUpper() && u.Tenant_Id == TenantId);
-        } 
-        #endregion
-    }
-
-    /// <summary>
-    /// Client DB Tenant
-    /// </summary>
     public class Tenant
     {
         public int Id { get; set; }
